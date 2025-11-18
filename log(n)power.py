e84b2b19e135ce2c242ad1e9d8d1e44ab6cab143
wajihakanwalgit <vajeeha.kanwal25@gmail.com>
2025-11-18 11:18:55 +0500

    Add files via upload

---
+++ log(n)power.py
@@ -0,0 +1,14 @@
+def computepower(x ,y):
+    result=1
+    while(y>0):
+        if(y%2==0):
+            x=x*x
+            y>>=1
+        else:
+            result=result*x
+            y=y-1
+    return result
+x=int(input("Enter x for x^y:"))
+y=int(input("Enter y for x^y:"))
+print("Total : ",computepower(x,y))
+
